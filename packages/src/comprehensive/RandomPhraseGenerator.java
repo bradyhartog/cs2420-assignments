772ed90191228183a6ad7d3cfad59d6d3bacd169
VivekVankayalapati <vivekvankayalapati@gmail.com>
2020-04-11 07:43:04 -0600

    Testing stringbuilder

--- packages/src/comprehensive/RandomPhraseGenerator.java
+++ packages/src/comprehensive/RandomPhraseGenerator.java
@@ -8,6 +8,13 @@ public class RandomPhraseGenerator
 {
     public static void main (String[] args)
     {
-        RandomPhrase generator = new RandomPhrase(Integer.parseInt(args[0]));
+        //RandomPhrase generator = new RandomPhrase(Integer.parseInt(args[0]));
+
+        //String builder can extend a string, thank goodness
+        // StringBuilder string = new StringBuilder("this is a word");
+
+        // string.replace(5, 6, "do be like that");
+
+        // System.out.println(string.toString());
     }
 }
